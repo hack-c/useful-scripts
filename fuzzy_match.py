7d8620f94c6f06b4c6929d104903c2ba2153b108
Charles Hack <charles.hack@accenture.com>
2017-06-23 17:55:27 -0400

    update fuzzy_match

--- fuzzy_match.py
+++ fuzzy_match.py
@@ -1,18 +1,47 @@
+# Fuzzy Match
+# Charlie Hack
+# <charles.hack@accenture.com>
+# January 2017
+#
+# Fuzzy match `input` (single-column text file) to `choices`, a single-column text file of possible choices.
+# Output file is same # of rows as `input` in the format:
+#
+# [Input value 1],[Closest match from `choices`],[Confidence 0-100]
+# ...
+# 
+# It's a good idea to filter out matches with confidence < 90. 
+#
+#
+
+import click
 import pandas as pd
 from fuzzywuzzy import process
 
-choices = list(pd.read_csv('~/projects/coned/ot_analytics/central/case_studies/maximo_location_id.csv', header=None).iloc[:,0])
-toms    = list(pd.read_csv('~/projects/coned/ot_analytics/central/case_studies/toms_location_equip.csv', header=None).iloc[:,0])
+date = datetime.now().strftime('%Y%m%d') # for output filenames
 
-with open('/Users/charles.hack/projects/coned/ot_analytics/central/case_studies/toms_maximo_mapping.csv','w') as outfile:
-    for x in toms[1053:]:
-        try:
-            choice, confidence = process.extractOne(x, choices)
-        except:
-            print x, 'FAILED'
-            outfile.write(','.join((str(x), 'FAILURE', '0')))
+
+@click.command()
+@click.argument('input_path', type=click.Path(exists=True))
+@click.argument('choices_path', type=click.Path(exists=True))
+@click.option('--outpath', '-o', type=click.Path(exists=False), default=None)
+def main(input_path, choices_path, outpath=None):
+
+    choices = list(pd.read_csv(choices_path, header=None).iloc[:,0])
+    input   = list(pd.read_csv(input_path, header=None).iloc[:,0])
+
+    if outpath is None:
+        orig, ext = os.path.splitext(choices_path)
+        outpath = orig + '_Fuzzy_Mapped_v{}'.format(date) + ext
+
+    with open(outpath,'w') as outfile:
+        for x in input:
+            try:
+                choice, confidence = process.extractOne(x, choices)
+            except:
+                print x, 'FAILED'
+                outfile.write(','.join((str(x), 'FAILURE', '0')))
+                outfile.write('\n')
+                continue
+            print x, '-->', choice, confidence
+            outfile.write(','.join((x, choice, str(confidence))))
             outfile.write('\n')
-            continue
-        print x, '-->', choice, confidence
-        outfile.write(','.join((x, choice, str(confidence))))
-        outfile.write('\n')
